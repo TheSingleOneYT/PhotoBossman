2bbb90ca110fd7d20be21b307a7f4d7321d51847
TheSingleOne <81616417+TheSingleOneYT@users.noreply.github.com>
2021-09-11 18:18:28 +0100

    Add 1.0

---
+++ PhotoBossman/Modules/Other.cs
@@ -0,0 +1,150 @@
+﻿using Discord;
+using Discord.Commands;
+using Microsoft.VisualBasic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace PhotoBossman.Modules
+{
+    public class Other : ModuleBase<SocketCommandContext>
+    {
+        public static string LoremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, " +
+                "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam" +
+                ", quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. " +
+                "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat " +
+                "nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia " +
+                "deserunt mollit anim id est laborum.";
+
+        //Owner commands
+
+        [Command("clear")]
+        [RequireOwner]
+        public async Task ClearImgsAsync()
+        {
+            await Context.Channel.SendMessageAsync("Clearing images...");
+
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = Directory.GetCurrentDirectory() + "/PhotoBossman",
+                UseShellExecute = true
+            });
+
+            var processes = Process.GetProcessesByName("PhotoBossman");
+
+            foreach (var process in processes)
+            {
+                process.Kill();
+            }
+        }
+
+        [Command("cfu")]
+        [RequireOwner]
+        public async Task UpdateCheckAsync()
+        {
+            await Context.Channel.SendMessageAsync("Checking for updates...");
+            await Context.Channel.SendMessageAsync("Results have been DM'd to you " + Context.Message.Author.Mention);
+            await Context.User.SendMessageAsync("Could not find Github file specified.");
+        }
+
+        [Command("sd")]
+        [Alias("shut-down", "kill", "end")]
+        [RequireOwner]
+        public async Task EndTaskAsync()
+        {
+            await Context.Channel.SendMessageAsync("Ending process...");
+
+            var processes = Process.GetProcessesByName("PhotoBossman");
+
+            foreach (var process in processes)
+            {
+                process.Kill();
+            }
+        }
+
+        [Command("complete-request")]
+        [Alias("cr")]
+        [RequireOwner]
+        public async Task AddedSuggestAsync([Remainder] string s)
+        {
+            var builder = new EmbedBuilder()
+                .WithTitle("Added!")
+                .WithDescription(s)
+                .WithCurrentTimestamp()
+                .WithColor(new Color(68, 255, 0));
+
+            var embed = builder.Build();
+
+            await Context.Message.Channel.SendMessageAsync(embed: embed);
+
+            await Context.Message.DeleteAsync();
+        }
+
+        //Start of non owner commands
+        [Command("help")]
+        [Alias("h", "info", "howto", "use")]
+        public async Task HelpAsync()
+        {
+            await Context.Channel.SendMessageAsync("To use Photo Bossman, use w/ {AMOUNT} to stretch the width or h/ {AMOUNT} to stretch the height.\n" +
+                "- Make sure to attach an image for the bot to stretch!\n\n" +
+                "Use g/ to get the link to a Discord photo.\n" +
+                "- If you are using this image as a link, add 'https://' to the begining of the link!\n\n" +
+                "Use i/ to invert an attached image.\n\n" +
+                "Use gs/ to make an attached image greyscale.\n\n" +
+                "FAQ:\n" +
+                "1) I can't use !sd, !cfu or !cr?\n- Those are server owner commands. If you are not the server owner, you cannot use them.\n" +
+                "2) The bot won't reply with an image!\n- Use !restart or !reset then try again. If still not, use a different amount.\n" +
+                "3) Can I download the source code?\n Soon, just going to finish the 1st release, that is also why !cfu is incomplete.");
+        }
+
+        [Command("restart")]
+        [Alias("reset")]
+        public async Task RestartTaskAsync()
+        {
+            await Context.Message.Channel.SendMessageAsync("Restarting...");
+
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = Directory.GetCurrentDirectory() + "/PhotoBossman.exe",
+                UseShellExecute = true
+            });
+
+            var processes = Process.GetProcessesByName("PhotoBossman");
+
+            foreach (var process in processes)
+            {
+                process.Kill();
+            }
+        }
+
+        [Command("suggest")]
+        [Alias("request")]
+        public async Task SuggestAsync([Remainder] string s)
+        {
+            var builder = new EmbedBuilder()
+                .WithAuthor(Context.Message.Author)
+                .WithTitle("Suggestion!")
+                .WithDescription(s)
+                .WithCurrentTimestamp()
+                .WithFooter("Suggestion for PhotoBossman")
+                .WithColor(new Color(122, 180, 192));
+
+            var embed = builder.Build();
+
+            var sent = await Context.Message.Channel.SendMessageAsync(embed: embed);
+            await sent.AddReactionAsync(new Emoji("😍"));
+            await sent.AddReactionAsync(new Emoji("💩"));
+
+            await Context.Message.DeleteAsync();
+        }
+
+        [Command("ver")]
+        public async Task VersionAsync()
+        {
+            await Context.User.SendMessageAsync("Version: " + Assembly.GetExecutingAssembly().GetName().Version.ToString());
+            await Context.Channel.SendMessageAsync("I have DM'd you " + Context.User.Mention);
+        }
+    }
+}
---
+++ PhotoBossman/Services/CommandHandler.cs
@@ -0,0 +1,283 @@
+﻿using Discord.Addons.Hosting;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PhotoBossman.Services
+{
+    public class CommandHandler : InitializedService
+    {
+        private readonly IServiceProvider provider;
+        private readonly DiscordSocketClient client;
+        private readonly CommandService service;
+        private readonly IConfiguration configuration;
+
+        Bitmap bmp;
+
+        public CommandHandler(IServiceProvider provider, DiscordSocketClient client, CommandService service, IConfiguration configuration)
+        {
+            this.provider = provider;
+            this.service = service;
+            this.client = client;
+            this.configuration = configuration;
+        }
+
+        public override async Task InitializeAsync(CancellationToken cancellationToken)
+        {
+            this.client.MessageReceived += OnMessageReceived;
+            this.service.CommandExecuted += OnCommandExecuted;
+            await this.service.AddModulesAsync(Assembly.GetEntryAssembly(), this.provider);
+        }
+
+        private async Task OnCommandExecuted(Discord.Optional<CommandInfo> commandInfo, ICommandContext commandContext, IResult result)
+        {
+            if (result.IsSuccess)
+                return;
+
+            if (result.ErrorReason == "Unknown command.")
+            {
+                return;
+            }
+
+            await commandContext.Channel.SendMessageAsync(result.ErrorReason);
+        }
+
+        private async Task OnMessageReceived(SocketMessage socketMessage)
+        {
+            if (!(socketMessage is SocketUserMessage message)) return;
+            if (message.Source != Discord.MessageSource.User) return;
+
+            if (message.Content.StartsWith("h/"))
+            {
+                await message.Channel.TriggerTypingAsync();
+
+                await message.Channel.SendMessageAsync(message.Author.Mention + ", preparing image...");
+
+                await message.Channel.TriggerTypingAsync();
+
+                var amount = Int32.Parse(message.Content.Remove(0, 3));
+
+                var url = message.Attachments.FirstOrDefault().Url;
+
+                var wc = new WebClient();
+                wc.DownloadFile(url, Directory.GetCurrentDirectory() + "/img.png");
+
+                Image img = Image.FromFile(Directory.GetCurrentDirectory() + "/img.png");
+                int orgHeight = img.Height;
+
+                bmp = new Bitmap(img.Width, orgHeight * amount);
+
+                Resize(img, img.Width, orgHeight * amount);
+
+                await message.Channel.SendFileAsync(Directory.GetCurrentDirectory() + "/output.png", message.Author.Mention);
+
+                File.Delete(Directory.GetCurrentDirectory() + "/output.png");
+
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = Directory.GetCurrentDirectory() + "/PhotoBossman",
+                    UseShellExecute = true
+                });
+
+                var processes = Process.GetProcessesByName("PhotoBossman");
+
+                foreach (var process in processes)
+                {
+                    process.Kill();
+                }
+            }
+            else if (message.Content.StartsWith("w/"))
+            {
+                await message.Channel.TriggerTypingAsync();
+
+                await message.Channel.SendMessageAsync(message.Author.Mention + ", preparing image...");
+
+                await message.Channel.TriggerTypingAsync();
+
+                var amount = Int32.Parse(message.Content.Remove(0, 3));
+
+                var url = message.Attachments.FirstOrDefault().Url;
+
+                var wc = new WebClient();
+                wc.DownloadFile(url, Directory.GetCurrentDirectory() + "/img.png");
+
+                Image img = Image.FromFile(Directory.GetCurrentDirectory() + "/img.png");
+                int orgHeight = img.Height;
+
+                bmp = new Bitmap(img.Width * amount, orgHeight);
+
+                Resize(img, img.Width * amount, orgHeight);
+
+                await message.Channel.SendFileAsync(Directory.GetCurrentDirectory() + "/output.png", message.Author.Mention);
+
+                File.Delete(Directory.GetCurrentDirectory() + "/output.png");
+
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = Directory.GetCurrentDirectory() + "/PhotoBossman",
+                    UseShellExecute = true
+                });
+
+                var processes = Process.GetProcessesByName("PhotoBossman");
+
+                foreach (var process in processes)
+                {
+                    process.Kill();
+                }
+            }
+            else if (message.Content.StartsWith("g/"))
+            {
+                await message.Channel.TriggerTypingAsync();
+
+                await message.Channel.SendMessageAsync(message.Author.Mention + ", generating link...");
+
+                await message.Channel.TriggerTypingAsync();
+
+                var link = message.Attachments.FirstOrDefault().Url.Remove(0, 8);
+
+                await message.Channel.SendMessageAsync(link + "\n(Add https:// to the beginning)");
+            }
+            else if (message.Content.StartsWith("i/"))
+            {
+                await message.Channel.TriggerTypingAsync();
+
+                await message.Channel.SendMessageAsync(message.Author.Mention + ", preparing image...");
+
+                await message.Channel.TriggerTypingAsync();
+
+                var url = message.Attachments.FirstOrDefault().Url;
+
+                var wc = new WebClient();
+                wc.DownloadFile(url, Directory.GetCurrentDirectory() + "/img.png");
+
+                Image img = Image.FromFile(Directory.GetCurrentDirectory() + "/img.png");
+
+                Invert(img);
+
+                await message.Channel.SendFileAsync(Directory.GetCurrentDirectory() + "/output.png", message.Author.Mention);
+
+                File.Delete(Directory.GetCurrentDirectory() + "/output.png");
+
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = Directory.GetCurrentDirectory() + "/PhotoBossman",
+                    UseShellExecute = true
+                });
+
+                var processes = Process.GetProcessesByName("PhotoBossman");
+
+                foreach (var process in processes)
+                {
+                    process.Kill();
+                }
+            }
+            else if (message.Content.StartsWith("gs/"))
+            {
+                await message.Channel.TriggerTypingAsync();
+
+                await message.Channel.SendMessageAsync(message.Author.Mention + ", preparing image...");
+
+                await message.Channel.TriggerTypingAsync();
+
+                var url = message.Attachments.FirstOrDefault().Url;
+
+                var wc = new WebClient();
+                wc.DownloadFile(url, Directory.GetCurrentDirectory() + "/img.png");
+
+                Image img = Image.FromFile(Directory.GetCurrentDirectory() + "/img.png");
+
+                gs(img);
+
+                await message.Channel.SendFileAsync(Directory.GetCurrentDirectory() + "/output.png", message.Author.Mention);
+
+                File.Delete(Directory.GetCurrentDirectory() + "/output.png");
+
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = Directory.GetCurrentDirectory() + "/PhotoBossman",
+                    UseShellExecute = true
+                });
+
+                var processes = Process.GetProcessesByName("PhotoBossman");
+
+                foreach (var process in processes)
+                {
+                    process.Kill();
+                }
+            }
+            else
+            {
+                int argPos = 0;
+
+                if (!message.HasStringPrefix(this.configuration["Prefix"], ref argPos) && !message.HasMentionPrefix(this.client.CurrentUser, ref argPos)) return;
+
+                var context = new SocketCommandContext(this.client, message);
+                await this.service.ExecuteAsync(context, argPos, this.provider);
+            }
+        }
+
+        Image Resize(Image image, int width, int height)
+        {
+            Graphics graphic = Graphics.FromImage(bmp);
+            graphic.DrawImage(image, 0, 0, width, height);
+            graphic.Dispose();
+
+            bmp.Save(Directory.GetCurrentDirectory() + "/output.png", ImageFormat.Png);
+
+            return bmp;
+        }
+
+        Bitmap newBitmap;
+
+        Image Invert(Image image) // from: https://youtu.be/AqRepVUMFs4
+        {
+            newBitmap = new Bitmap(image);
+            for (int x = 0; x < newBitmap.Width; x++)
+            {
+                for (int y = 0; y < newBitmap.Height; y++)
+                {
+                    Color pixel = newBitmap.GetPixel(x, y);
+
+                    int red = pixel.R;
+                    int green = pixel.G;
+                    int blue = pixel.B;
+
+                    newBitmap.SetPixel(x, y, Color.FromArgb(255 - red, 255 - green, 255 - blue));
+                }
+
+            }
+            newBitmap.Save(Directory.GetCurrentDirectory() + "/output.png", ImageFormat.Png);
+            return newBitmap;
+        }
+
+        Image gs(Image image) // from: https://youtu.be/Ww1BAL8Yh_Y
+        {
+            newBitmap = new Bitmap(image);
+            for (int x = 0; x < newBitmap.Width; x++)
+            {
+                for (int y = 0; y < newBitmap.Height; y++)
+                {
+                    Color original = newBitmap.GetPixel(x, y);
+
+                    int greyScale = (int)((original.R * .3) + (original.G * .59) + (original.B * .11));
+
+                    Color newcolor = Color.FromArgb(greyScale, greyScale, greyScale);
+
+                    newBitmap.SetPixel(x, y, newcolor);
+                }
+            }
+            newBitmap.Save(Directory.GetCurrentDirectory() + "/output.png", ImageFormat.Png);
+            return newBitmap;
+        }
+    }
+}
